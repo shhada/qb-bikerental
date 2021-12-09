8142af958017bafbb01298e7e892f625cc977148
shhada <shariff93@hotmail.co.uk>
2021-12-09 04:41:58 +0100

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,24 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local bikeprice = {}
 
+QBCore.Functions.CreateCallback('qb-bikerental:checkPrice', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local CitizenId = Player.PlayerData.citizenid
+
+    if Player.PlayerData.money.bank >= Config.BikePrice then
+        bikeprice[CitizenId] = "bank"
+        Player.Functions.RemoveMoney('bank', Config.BikePrice)
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
+RegisterServerEvent('qb-bikerental:hasMoney', function(hasMoney)
+    local user = source
+    local Player = QBCore.Functions.GetPlayer(user)
+
+    if Player then
+        Player.Functions.RemoveMoney('bank', hasMOney)
+    end
+end)  
