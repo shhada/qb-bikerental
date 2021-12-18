aa60e16cb698a3e3388a76a9eff9c33bfda795f6
shhada <shariff93@hotmail.co.uk>
2021-12-18 21:53:04 +0100

    work in progress

--- config.lua
+++ config.lua
@@ -1,3 +1,38 @@
 Config = {}
 
-Config.BikePrice = 150  -- price in usd $
+Config.Price = 150  -- price in usd $
+
+Config.Bikes = {
+    ['bmx'] = {
+        ['model'] = 'bmx',
+        ['description'] = "Shred in the park",
+    },
+    ['cruiser'] = {
+        ['model'] = 'cruiser',
+        ['description'] = "Cruise on the beach",
+    },
+    ['fixter'] = {
+        ['model'] = 'fixter',
+        ['description'] = "Fixed gear bike",
+    },
+    ['scorcher'] = {
+        ['model'] = 'scorcher',
+        ['description'] = "Mountain Bike",
+    },
+    ['tribike'] = {
+        ['model'] = 'tribike',
+        ['description'] = "Triathlon bike in GREEN and WHITE",
+    },
+    ['tribike2'] = {
+        ['model'] = 'tribike2',
+        ['description'] = "Triathlon bike in RED and WHITE",
+    },
+    ['tribike3'] = {
+        ['model'] = 'tribike3',
+        ['description'] = "Triathlon bike in BLUE and WHITE",
+    },
+    ['electricscooter'] = {
+        ['model'] = 'electricscooter',
+        ['description'] = "for those times when you're too lazy",
+    }
+}
